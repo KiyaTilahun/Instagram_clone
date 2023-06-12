5f9aed7f14983ba7a3925cb096512bb762faf4bc
KiyaTilahun <kiyatilahun0@gmail.com>
2023-06-12 07:44:35 +0300

    this is instagram cloning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="images/kiya.jpg" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div>
+                <h1>kiya_tilahun_lemmi</h1>
             </div>
+            <div class="d-flex ">
+                <div class="pe-5"><strong>0</strong> posts</div>
+                <div class="pe-5"><strong>84</strong> followers</div>
+                <div class="pe-5"><strong>100</strong> following</div>
+            </div>
+            <div class="fw-bold">
+                <div>kiya _tilahun_lemmi</div>
+                <div>መወለድ ቋንቋ ነው</div>
+                <div>🇪🇹Proud Ethiopian</div>
+                <div><a href="mailto:=kiyatilahun0@gmail.com">Send Email</a></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4"><img src="images/insta1.jpg" alt="" class="w-100"></div>
+            <div class="col-4"><img src="images/insta1.jpg" alt="" class="w-100"></div>
+            <div class="col-4"><img src="images/insta1.jpg" alt="" class="w-100"></div>
+        </div>
+         <div class="row">
+            <div class="col-4"><img src="images/image-1.jpg" alt="" class="w-100 pt-5"></div>
+            <div class="col-4 pt-5"><img src="images/image-2.jpg" alt="" class="w-100"></div>
+            <div class="col-4 pt-5"><img src="images/image-3.jpg" alt="" class="w-100"></div>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Instagram
+               
+
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                   
+                <div ><img src="svg/instagram-seeklogo.com.svg" alt="" style="height:25px;border-right:1px solid #333" class="pe-3" ></div>
+                <div  class="ps-3">Instagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
